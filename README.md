3101c4d7da879462749872f66864d177ef778f38
Jan Majer <33447429+MajerJ@users.noreply.github.com>
2017-11-07 20:56:16 +0100

    Initial upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# ToDo 2.0
+
+Simple ToDo list created using Angular JS
+
+## Getting Started
+
+ToDo API must be running on localhost.
+
+### Prerequisites
+
+ToDo API
+
+
+## Authors
+
+* **Jan Majer** 
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ javascript/controllers/task-list.js
@@ -0,0 +1,70 @@
+angular.module('TaskList', [])
+.controller('MyTaskList', function($scope, $http, $httpParamSerializerJQLike) {
+    $scope.list = [];
+    $http({
+        method: 'GET',
+        url: 'http://localhost:3000/tasks',
+    }).then(function(response) {
+        $scope.list = response.data;
+    });
+
+    $scope.newTask = {};    
+
+    $scope.addTask = function() {        
+        $http({                      
+            method: 'POST',
+            url: 'http://localhost:3000/tasks',            
+            data: $httpParamSerializerJQLike({task: $scope.newTask.task}),
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(function(response) {
+            $scope.list.push(response.data);                            
+        });
+        $scope.newTask = {}
+    }
+    
+    $scope.addByEnter = function(keyEvent) {
+        if (keyEvent.which === 13) {
+            keyEvent.preventDefault();
+            $scope.addTask();
+        }
+    };
+
+    $scope.removeSelected = function() {
+        for (var i = ($scope.list.length)-1; i >= 0 ; i--) {
+            if ($scope.list[i].done) {                
+                $http({                    
+                    method: 'DELETE',
+                    url: 'http://localhost:3000/tasks/' + $scope.list[i]._id                   
+                }).then(function(response) {                    
+                    console.log(response.data)                    
+                });
+            $scope.list.splice([i], 1);
+            }
+        }    
+    };
+
+    $scope.updateTask = function() {
+        for (var i = 0; i < ($scope.list.length); i++) {                            
+                $http({                    
+                    method: 'PUT',
+                    url: 'http://localhost:3000/tasks/' + $scope.list[i]._id,
+                    data: $httpParamSerializerJQLike({done: $scope.list[i].done}),
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                }).then(function(response) {                    
+                    console.log(response.data)
+                });
+        } 
+    };
+
+    $scope.removeByCtrlD = function(keyEvent) {
+        if (keyEvent.which === 68 && keyEvent.ctrlKey) {
+            keyEvent.preventDefault();
+            $scope.removeSelected();
+        }
+    };
+
+    $scope.sortBy = function(propertyName) {
+        $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+        $scope.propertyName = propertyName;
+    };    
+});
